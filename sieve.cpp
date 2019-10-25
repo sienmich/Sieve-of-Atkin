bc4c58afd0f3779b8588059bc25f29bf8731711b
sienmich <sienmich@gmail.com>
2019-10-25 19:56:15 +0200

    Adding all files.

---
+++ sieve.cpp
@@ -0,0 +1,9 @@
+//
+// Created by sienmich on 25.10.2019.
+//
+
+#include "sieve.h"
+
+std::vector<int>& primes(int bound) {
+
+}
---
+++ sieve.h
@@ -0,0 +1,12 @@
+//
+// Created by sienmich on 25.10.2019.
+//
+
+#include <vector>
+
+#ifndef SIEVE_OF_ATKIN_SIEVE_H
+#define SIEVE_OF_ATKIN_SIEVE_H
+
+std::vector<int>& primes(int bound);
+
+#endif //SIEVE_OF_ATKIN_SIEVE_H
